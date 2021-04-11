b6857c384cc2bdcf3146d662ee688a592fcf86dd
Linmiao Xu <linmiao.xu@gmail.com>
2021-04-10 23:08:35 -0400

    Use max compression

--- create_zip_files.sh
+++ create_zip_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip -r bt.lichess-puzzles-v1.zip lichess-puzzles-v1/
-zip -r bt.game-modes.zip game-modes/
+zip -9 -r bt.lichess-puzzles-v1.zip lichess-puzzles-v1/
+zip -9 -r bt.game-modes.zip game-modes/
